dcd80c1106615cc913c4eec29026ddd6f4993f8c
Aidan McClelland <a-meister88@outlook.com>
2023-10-12 14:25:27 -0400

    worked on #20

--- column.java
+++ column.java
@@ -1,3 +1,13 @@
-public class column {
-    
+import java.util.ArrayList;
+public class column extends project {
+    private String title;
+    ArrayList<Task> tasks;
+}
+
+public void addTask() {
+    return;
+}
+
+public void removeTask() {
+    return;
 }
