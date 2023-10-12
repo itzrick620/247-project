5c6fe3dee281fbd6d1862acc74ab21e58db0ade4
Spencer Goldfine <goldfis@email.sc.edu>
2023-10-12 14:14:04 -0400

    worked on #15

--- projectManagementSystem.java
+++ projectManagementSystem.java
@@ -1,3 +1,24 @@
 public class projectManagementSystem {
-    
+    private String user;
+    private String project;
+
+    public void login(String username, String password) {
+
+    }
+
+    public void signUp(String username, String password, String firstName, String lastName, String email) {
+
+    }
+
+    public void createProject(String name) {
+
+    }
+
+    public void addTaskToProject(Task task) {
+
+    }
+
+    public void makeProjectComment(String userInput) {
+        
+    }
 }
