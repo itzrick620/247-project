8cfba2987bb4b24da748ced35567cb45c3b22576
gbujoreanu <111786155+gbujoreanu@users.noreply.github.com>
2023-10-12 21:49:25 -0400

    Edited File Names

--- userList.java
+++ userList.java
@@ -1,15 +1,24 @@
 import java.util.ArrayList;
 
 public class UserList {
- 
-private ArrayList<User> users;
-private UserList UserList;
+    private ArrayList<User> users;
+    private static UserList userList;
 
+    private UserList() {}
 
+    public static UserList getInstance() {
+        return userList;
+    }
 
+    public User addUser(String username, String password, String firstName, String lastName, String email) {
+        return null;
+    }
 
+    public User removeUser(String username) {
+        return null;
+    }
 
-
-
-
+    public User getUser(String username) {
+        return null;
+    }
 }
