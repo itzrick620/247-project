8e991e6d0a1f8d2381488c1449e03360b9a94b16
SpencerG3 <spencergold7704@gmail.com>
2023-10-12 22:20:07 -0400

    fixed #17

---
+++ Database.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Database {
+    public ArrayList<User> getUsers() {
+        return null;
+    }
+
+    public ArrayList<Project> getProjects() {
+        return null;
+    }
+
+    public boolean saveUsers() {
+        return false;
+    }
+
+    public boolean saveProjects() {
+        return false;
+    }
+}
