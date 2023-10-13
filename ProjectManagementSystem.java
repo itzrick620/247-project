cc4869d508e823903b6551e86b104338c7b5c6de
SpencerG3 <spencergold7704@gmail.com>
2023-10-12 22:20:00 -0400

    fixed #15

---
+++ ProjectManagementSystem.java
@@ -0,0 +1,10 @@
+public class ProjectManagementSystem {
+   private User user;
+   private Project project;
+   
+   public void login(String username, String password) {}
+
+   public void signUp(String username, String password, String firstName, String lastName, String email) {}
+
+   public void createProject(String name) {}
+}
