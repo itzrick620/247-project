39c86470a9a95af8eab42953fe1469c7204e9326
Ricky Garretson <rickygeeiii@outlook.com>
2023-10-15 17:12:58 -0400

    #18 finish

--- Project.java
+++ Project.java
@@ -1,3 +1,23 @@
+import java.util.UUID;
+import java.util.ArrayList;
 public class Project {
-    
+    private UUID id;
+    private String name;
+    private String footnotes;
+    private User owner;
+    private User scrumMaster;
+    private ArrayList<Comment> comment;
+    private ArrayList<User> developers;
+
+    public Project(String name){}
+
+    public Project(UUID id, String name){}
+
+    public void addTask(Task task){}
+
+    public void removeTask(Task task){}
+
+    public void share(){}
+
+    public void addFootnotes(){}
 }
