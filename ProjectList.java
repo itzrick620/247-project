3cadbfe4d2c2ff33f824e44c57f7f8a7934128ac
Ricky Garretson <rickygeeiii@outlook.com>
2023-10-15 17:13:40 -0400

    #19 finish

--- ProjectList.java
+++ ProjectList.java
@@ -1,3 +1,21 @@
+import java.util.ArrayList;
 public class ProjectList {
+    private ArrayList<Project> projects;
+    private static ProjectList projectList;
     
+    public boolean addProject(String name){
+        return false;
+    }
+    
+    public boolean removeProject(String name){
+        return false;
+    }
+
+    public Project getProject(String name){
+        return null;
+    }
+
+    public ArrayList<Project> getAllProjects(){
+        return null;
+    }
 }
