17cd20fb75074a71e6e83ed05c78cb62b8500cb0
SpencerG3 <spencergold7704@gmail.com>
2023-10-16 19:16:09 -0400

    worked on #1

--- ProjectManagementSystem.java
+++ ProjectManagementSystem.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
+
 public class ProjectManagementSystem {
    private User user;
    private Project project;
    
-   public void login(String username, String password) {}
+   public void login(String username, String password) {
+      // Scanner keyboard = new Scanner (System.in);
+
+      // username = keyboard.nextLine();
+      // password = keyboard.nextLine();
+
+      // if(username.equals(user.getUsername())) {
+      //    System.out.println("Login Succesful");
+      // }
+      // else {
+      //    System.out.println("Login Error: Please Try again");
+      // }
+   }
 
    public void signUp(String username, String password, String firstName, String lastName, String email) {}
 
