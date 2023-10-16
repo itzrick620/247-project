91c2fd192a50d94b79edf56420bb7433c942ce91
gbujoreanu <111786155+gbujoreanu@users.noreply.github.com>
2023-10-16 16:14:43 -0400

    Implemented Methods #2

--- UserList.java
+++ UserList.java
@@ -1,24 +1,72 @@
 import java.util.ArrayList;
 
+/**
+ * Manages a list of users in the project management system.
+ */
 public class UserList {
-    private ArrayList<User> users;
-    private static UserList userList;
+    private ArrayList<User> users;  // List of users
+    private static UserList userListInstance;  // Singleton instance
 
-    private UserList() {}
+    private UserList() {
+        users = new ArrayList<>();
+    }
 
+    /**
+     * Gets the singleton instance of UserList.
+     *
+     * @return The UserList instance
+     */
     public static UserList getInstance() {
-        return userList;
+        if (userListInstance == null) {
+            userListInstance = new UserList();
+        }
+        return userListInstance;
     }
 
-    public User addUser(String username, String password, String firstName, String lastName, String email) {
-        return null;
+    /**
+     * Adds a new user to the list.
+     *
+     * @param username  User's username
+     * @param password  User's password
+     * @param firstName User's first name
+     * @param lastName  User's last name
+     * @param email     User's email address
+     * @return true if the user is added successfully, false otherwise
+     */
+    public boolean addUser(String username, String password, String firstName, String lastName, String email) {
+        User newUser = new User(username, password, firstName, lastName, email);
+        return users.add(newUser);
     }
 
-    public User removeUser(String username) {
-        return null;
+    /**
+     * Removes a user from the list.
+     *
+     * @param username The username of the user to remove
+     * @return true if the user is removed successfully, false otherwise
+     */
+    public boolean removeUser(String username) {
+        User userToRemove = null;
+        for (User user : users) {
+            if (user.getUsername().equals(username)) {
+                userToRemove = user;
+                break;
+            }
+        }
+        return userToRemove != null && users.remove(userToRemove);
     }
 
+    /**
+     * Gets a user by their username.
+     *
+     * @param username The username of the user to retrieve
+     * @return The User object if found, null otherwise
+     */
     public User getUser(String username) {
+        for (User user : users) {
+            if (user.getUsername().equals(username)) {
+                return user;
+            }
+        }
         return null;
     }
 }
