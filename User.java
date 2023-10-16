6f022d226e4fc8a4bce0c62d2ada76bb07d3262a
gbujoreanu <111786155+gbujoreanu@users.noreply.github.com>
2023-10-16 16:13:33 -0400

    Implemented Methods #13

--- User.java
+++ User.java
@@ -1,16 +1,63 @@
 import java.util.ArrayList;
 import java.util.UUID;
 
+/**
+ * Represents a user in the project management system.
+ */
 public class User {
-    private UUID id;
-    private String username;
-    private String password;
-    private String firstName;
-    private String lastName;
-    private String email;
-    private ArrayList<Project> projects;
+    private UUID id;            // Unique identifier for the user
+    private String username;    // User's username
+    private String password;    // User's password
+    private String firstName;   // User's first name
+    private String lastName;    // User's last name
+    private String email;       // User's email address
+    private ArrayList<Project> projects;  // Projects associated with the user
 
-    public User(String username, String password, String firstName, String lastName, String email, ArrayList<Project> projects) {}
+    /**
+     * Creates a new User instance.
+     *
+     * @param username  User's username
+     * @param password  User's password
+     * @param firstName User's first name
+     * @param lastName  User's last name
+     * @param email     User's email address
+     */
+    public User(String username, String password, String firstName, String lastName, String email) {
+        this.id = UUID.randomUUID();
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.projects = new ArrayList<>();
+    }
 
-    public User(UUID id, String username, String password, String firstName, String lastName, String email) {}
+    /**
+     * Creates a new User instance with a specified UUID.
+     *
+     * @param id        User's UUID
+     * @param username  User's username
+     * @param password  User's password
+     * @param firstName User's first name
+     * @param lastName  User's last name
+     * @param email     User's email address
+     */
+    public User(UUID id, String username, String password, String firstName, String lastName, String email) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.projects = new ArrayList<>();
+    }
+
+    /**
+     * Gets the username of the user.
+     *
+     * @return The username of the user
+     */
+    public String getUsername() {
+        return username;
+    }
 }
