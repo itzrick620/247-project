aae27e1d4cc8862b40f254a2e51ed38575bbdc8b
Spencer Goldfine <goldfis@email.sc.edu>
2023-10-17 13:00:05 -0400

    fixed file name #20

---
+++ Column.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Column {
+    private String title;
+    private ArrayList<Task> tasks;
+
+    public void addTask(Task task) {}
+
+    public void removeTask(Task task) {}
+}
