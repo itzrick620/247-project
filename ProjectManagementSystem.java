e691c7394f5a15d75af2c039e50a5c086ce8d89e
gbujoreanu <111786155+gbujoreanu@users.noreply.github.com>
2023-10-17 20:30:07 -0400

    Added Implementaion Sign up & Login #1

--- ProjectManagementSystem.java
+++ ProjectManagementSystem.java
@@ -1,26 +1,62 @@
 import java.util.Scanner;
 
 public class ProjectManagementSystem {
-   private User user;
-   private Project project;
+    private User user;
+    private Project project;
+    private UserList userList;  // Add the UserList instance
 
-   // User login
-   public User login(String username, String password) {
-      return null;
-  }
+    public ProjectManagementSystem() {
+        userList = UserList.getInstance();  // Initialize the UserList instance
+    }
 
-  // User sign-up
-  public User signUp(String username, String password, String firstName, String lastName, String email) {
-      return null; // Sign-up failed
-  }
+    public void login() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter username: ");
+        String username = scanner.nextLine();
+        System.out.print("Enter password: ");
+        String password = scanner.nextLine();
 
-   public void createProject(String name) {}
+        // Check if the user exists and validate password
+        User loggedInUser = userList.getUsers(username);
+        if (loggedInUser != null && loggedInUser.getPassword().equals(password)) {
+            System.out.println("Login successful.");
+            user = loggedInUser;  // Set the logged-in user
+        } else {
+            System.out.println("Login failed. Please try again.");
+        }
+    }
 
-   public boolean addTaskToProject(Task task) {
-      return false;
-   }
-
-   public void makeProjectComment(String userInput) {
+    public void signUp() {
+      Scanner scanner = new Scanner(System.in);
       
-   }
+      System.out.print("Enter username: ");
+      String username = scanner.nextLine();
+      
+      System.out.print("Enter password: ");
+      String password = scanner.nextLine();
+      
+      System.out.print("Enter first name: ");
+      String firstName = scanner.nextLine();
+      
+      System.out.print("Enter last name: ");
+      String lastName = scanner.nextLine();
+      
+      System.out.print("Enter email: ");
+      String email = scanner.nextLine();
+      
+      // Validate input data and add the user to the UserList
+      if (username.isEmpty() || password.isEmpty() || firstName.isEmpty() || lastName.isEmpty() || email.isEmpty()) {
+          System.out.println("Signup failed. Please provide all required information.");
+          return;
+      }
+  
+      if (userList.addUser(username, password, firstName, lastName, email)) {
+          System.out.println("Signup successful.");
+      } else {
+          System.out.println("Signup failed. Username already exists.");
+      }
+  }
+
+    public void createProject(String name) {
+    }
 }
