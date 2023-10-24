23b2aece4219f333d36757e39bf1770eeada2ce5
Spencer Goldfine <goldfis@email.sc.edu>
2023-10-24 14:06:07 -0400

    worked on #1

--- ProjectManagementSystem.java
+++ ProjectManagementSystem.java
@@ -1,72 +1,82 @@
 import java.util.Scanner;
 
 public class ProjectManagementSystem {
-    private User user;
-    private Project project;
+    private boolean user;
+    private boolean project;
     private UserList userList;  // Add the UserList instance
 
     public ProjectManagementSystem() {
         userList = UserList.getInstance();  // Initialize the UserList instance
     }
 
-    public void login() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter username: ");
-        String username = scanner.nextLine();
-        System.out.print("Enter password: ");
-        String password = scanner.nextLine();
+    public boolean login(String username) {
 
-        // Check if the user exists and validate password
-        User loggedInUser = userList.getUser(username);
-        if (loggedInUser != null && loggedInUser.getPassword().equals(password)) {
-            System.out.println("Login successful.");
-            user = loggedInUser;  // Set the logged-in user
-        } else {
-            System.out.println("Login failed. Please try again.");
-        }
+        user = UserList.getInstance().getUser(username);
+        return user;
     }
 
-    public void signUp() {
-      Scanner scanner = new Scanner(System.in);
-      
-      System.out.print("Enter username: ");
-      String username = scanner.nextLine();
-      
-      System.out.print("Enter password: ");
-      String password = scanner.nextLine();
-      
-      System.out.print("Enter first name: ");
-      String firstName = scanner.nextLine();
-      
-      System.out.print("Enter last name: ");
-      String lastName = scanner.nextLine();
-      
-      System.out.print("Enter email: ");
-      String email = scanner.nextLine();
-      
-      // Validate input data and add the user to the UserList
-      if (username.isEmpty() || password.isEmpty() || firstName.isEmpty() || lastName.isEmpty() || email.isEmpty()) {
-          System.out.println("Signup failed. Please provide all required information.");
-          return;
-      }
-  
-      if (userList.addUser(username, password, firstName, lastName, email)) {
-          System.out.println("Signup successful.");
-      } else {
-          System.out.println("Signup failed. Username already exists.");
-      }
-
-      scanner.close();
+    public boolean signUp(String username, String password, String firstName, String lastName, String email) {
+        user = UserList.getInstance().addUser(username, password, firstName, lastName, email);
+        return user;
   }
 
-    public void createProject(String name) {
+    public boolean createProject(String name) {
+        project = ProjectList.getInstance().addProject(name);
     }
 
     public boolean addTaskToProject(Task task) {
-        return false;
+        project = Project.addTask(task);
     }
 
     public void makeProjectComment(String userInput) {
         
     }
+
+    //login
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.print("Enter username: ");
+        // String username = scanner.nextLine();
+        // System.out.print("Enter password: ");
+        // String password = scanner.nextLine();
+
+        // // Check if the user exists and validate password
+        // User loggedInUser = userList.getUser(username);
+        // if (loggedInUser != null && loggedInUser.getPassword().equals(password)) {
+        //     System.out.println("Login successful.");
+        //     user = loggedInUser;  // Set the logged-in user
+        // } else {
+        //     System.out.println("Login failed. Please try again.");
+        // }
+
+    //signup
+        //   Scanner scanner = new Scanner(System.in);
+        
+        //   System.out.print("Enter username: ");
+        //   String username = scanner.nextLine();
+        
+        //   System.out.print("Enter password: ");
+        //   String password = scanner.nextLine();
+        
+        //   System.out.print("Enter first name: ");
+        //   String firstName = scanner.nextLine();
+        
+        //   System.out.print("Enter last name: ");
+        //   String lastName = scanner.nextLine();
+        
+        //   System.out.print("Enter email: ");
+        //   String email = scanner.nextLine();
+        
+        //   // Validate input data and add the user to the UserList
+        //   if (username.isEmpty() || password.isEmpty() || firstName.isEmpty() || lastName.isEmpty() || email.isEmpty()) {
+        //       System.out.println("Signup failed. Please provide all required information.");
+        //       return;
+        //   }
+    
+        //   if (userList.addUser(username, password, firstName, lastName, email)) {
+        //       System.out.println("Signup successful.");
+        //   } else {
+        //       System.out.println("Signup failed. Username already exists.");
+        //   }
+
+        //   scanner.close();
 }
