67a5396aa85193c836c83fa52161065c242134cf
gbujoreanu <111786155+gbujoreanu@users.noreply.github.com>
2023-10-31 01:37:31 -0400

    Fixed #7

--- Column.java
+++ Column.java
@@ -1,19 +1,97 @@
 import java.util.ArrayList;
+import java.util.UUID;
 
+/**
+ * Represents a column in a project management system, holding a list of tasks.
+ */
 public class Column {
-    private String title;
-    private ArrayList<Task> tasks;
 
+    
+    private final String title; // The title of the column
+    private final ArrayList<Task> tasks; // The list of tasks in the column
+    
+
+    /**
+     * Constructs a new Column with the given title.
+     *
+     * @param title the title of the column
+     */
     public Column(String title) {
         this.title = title;
         this.tasks = new ArrayList<>();
     }
 
+    /**
+     * Adds a task to the column.
+     *
+     * @param task the task to be added
+     */
     public void addTask(Task task) {
         tasks.add(task);
     }
 
-    public void removeTask(Task task) {
-        tasks.remove(task);
+    /**
+     * Removes a task from the column based on its UUID.
+     *
+     * @param taskId the UUID of the task to be removed
+     * @return true if the task was removed successfully, false otherwise
+     */
+    public boolean removeTask(UUID taskId) {
+        return tasks.removeIf(task -> task.getId().equals(taskId));
+    }
+
+    /**
+     * Updates a task in the column based on its UUID.
+     *
+     * @param taskId      the UUID of the task to be updated
+     * @param updatedTask the updated task
+     * @return true if the task was updated successfully, false otherwise
+     */
+    public boolean updateTask(UUID taskId, Task updatedTask) {
+        int index = -1;
+        for (int i = 0; i < tasks.size(); i++) {
+            if (tasks.get(i).getId().equals(taskId)) {
+                index = i;
+                break;
+            }
+        }
+        if (index != -1) {
+            tasks.set(index, updatedTask);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Retrieves a task from the column based on its UUID.
+     *
+     * @param taskId the UUID of the task to retrieve
+     * @return the task if found, null otherwise
+     */
+    public Task getTaskById(UUID taskId) {
+        for (Task task : tasks) {
+            if (task.getId().equals(taskId)) {
+                return task;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Gets the tasks in the column.
+     *
+     * @return a list of tasks in the column
+     */
+    public ArrayList<Task> getTasks() {
+        return new ArrayList<>(tasks);
+    }
+
+    /**
+     * Gets the title of the column.
+     *
+     * @return the title of the column
+     */
+    public String getTitle() {
+        return title;
     }
 }
