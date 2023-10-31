e62aed1143e053d39228d4a1c7948681eae66530
gbujoreanu <111786155+gbujoreanu@users.noreply.github.com>
2023-10-31 01:38:53 -0400

    Fixed #9

--- Comment.java
+++ Comment.java
@@ -1,23 +1,28 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.UUID;
 
 /**
- * The Comment class represents a comment with various properties, including user input, user information, date, and replies.
+ * Represents a comment associated with a project.
  */
 public class Comment {
-    private String userInput;
-    private User user;
-    private Date date;
-    ArrayList<Comment> replies;
+
+    private final UUID id; // Unique identifier for the comment
+    private final String userInput; // The text content of the comment
+    private final User user; // The user who posted the comment
+    private Date date; // The date and time when the comment was posted
+    private final ArrayList<Comment> replies; // Replies to this comment
 
     /**
-     * Constructs a new Comment object with the given user input and user information.
-     * The date is automatically set to the current date, and there are no initial replies.
+     * Constructs a new Comment object with the given user input and user.
      *
-     * @param userInput The text of the comment provided by the user.
-     * @param user     The user who posted the comment.
+     * @param userInput the text content of the comment
+     * @param user       the user who posted the comment
      */
     public Comment(String userInput, User user) {
+        this.id = UUID.randomUUID();
         this.userInput = userInput;
         this.user = user;
         this.date = new Date();
@@ -25,90 +30,102 @@ public class Comment {
     }
 
     /**
-     * Constructs a new Comment object with the given user input, user information, date, and replies.
+     * Constructs a new Comment object with the given user input, user, date, and replies.
      *
-     * @param userInput The text of the comment provided by the user.
-     * @param user     The user who posted the comment.
-     * @param date     The date and time when the comment was posted.
-     * @param replies  A list of replies to this comment.
+     * @param userInput the text content of the comment
+     * @param user       the user who posted the comment
+     * @param date       the date and time when the comment was posted
+     * @param replies    replies to this comment
      */
     public Comment(String userInput, User user, Date date, ArrayList<Comment> replies) {
+        this.id = UUID.randomUUID();
         this.userInput = userInput;
         this.user = user;
-        this.date = date;
-        this.replies = replies;
+        this.date = new Date(date.getTime());
+        this.replies = new ArrayList<>(replies);
     }
 
     /**
-     * Gets the user input of the comment.
+     * Gets the unique identifier of the comment.
      *
-     * @return The text of the comment provided by the user.
+     * @return the UUID of the comment
      */
-    public String getUserInput() {
-        return userInput;
+    public UUID getId() {
+        return id;
+    }
+
+    /**
+     * Gets the date and time when the comment was posted.
+     *
+     * @return the date and time of the comment
+     */
+    public Date getDate() {
+        return new Date(date.getTime());
     }
 
     /**
      * Gets the user who posted the comment.
      *
-     * @return The user who posted the comment.
+     * @return the user who posted the comment
      */
     public User getUser() {
         return user;
     }
 
     /**
-     * Gets the date and time when the comment was posted.
-     *
-     * @return The date and time of the comment.
-     */
-    public Date getDate() {
-        return date;
-    }
-
-    /**
-     * Gets a list of replies to this comment.
-     *
-     * @return A list of comments that are replies to this comment.
-     */
-    public ArrayList<Comment> getReplies() {
-        return replies;
-    }
-
-    /**
-     * Sets the user input of the comment.
-     *
-     * @param userInput The text of the comment provided by the user.
-     */
-    public void setUserInput(String userInput) {
-        this.userInput = userInput;
-    }
-
-    /**
-     * Sets the user who posted the comment.
-     *
-     * @param user The user who posted the comment.
-     */
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    /**
      * Sets the date and time when the comment was posted.
      *
-     * @param date The date and time of the comment.
+     * @param date the date and time of the comment
      */
     public void setDate(Date date) {
-        this.date = date;
+        this.date = new Date(date.getTime());
     }
 
     /**
-     * Sets a list of replies to this comment.
+     * Sets the date and time of the comment from a string representation.
      *
-     * @param replies A list of comments that are replies to this comment.
+     * @param dateString the date and time of the comment as a string
+     * @throws ParseException if the date string cannot be parsed
      */
-    public void setReplies(ArrayList<Comment> replies) {
-        this.replies = replies;
+    public void setDate(String dateString) throws ParseException {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+        this.date = dateFormat.parse(dateString);
+    }
+
+    /**
+     * Retrieves the user input associated with this comment.
+     *
+     * @return A string representing the user's text input for this comment.
+     */
+    public String getUserInput() {
+        return userInput;
+    }
+
+    /**
+     * Adds a reply to this comment.
+     *
+     * @param reply the reply to add
+     */
+    public void addReply(Comment reply) {
+        replies.add(reply);
+    }
+
+    /**
+     * Retrieves the replies to this comment.
+     *
+     * @return an ArrayList of Comment objects representing the replies
+     */
+    public ArrayList<Comment> getReplies() {
+        return new ArrayList<>(replies);
+    }
+
+    /**
+     * Creates a sample comment for demonstration purposes.
+     *
+     * @return A sample Comment object.
+     */
+    public static Comment createSampleComment() {
+        User user = new User(UUID.randomUUID(), "username");
+        return new Comment("Some comment text", user);
     }
 }
-
